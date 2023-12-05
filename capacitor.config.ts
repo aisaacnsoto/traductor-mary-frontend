a2c9d07601859f5916366a60ca40388d9e37525c
Alonso Novoa <aisaacnsoto@gmail.com>
2023-12-05 17:07:06 -0500

    added appflow sdk

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,10 +2,18 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'photo-gallery',
+  appName: 'Traductor Mary',
   webDir: 'www',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    LiveUpdates: {
+      appId: 'fa2c1c4e',
+      channel: 'Production',
+      autoUpdateMethod: 'background',
+      maxVersions: 2
+    }
   }
 };
 
